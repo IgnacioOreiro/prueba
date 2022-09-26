45adf98246c71c849ed6be62523b441329a5f415
Ignacio Oreiro <ignaciooreiro3@outlook.es>
2022-09-26 18:37:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function saludoDiaDelProgramador(diaDelProgramador = 256){
+    if(diaDelProgramador = 256){
+        return 'Feliz dia a todos los programadores, prendanse un por*o, pidanse una pizza y abranse una birra'
+    }
+}
+console.log(saludoDiaDelProgramador(256))
